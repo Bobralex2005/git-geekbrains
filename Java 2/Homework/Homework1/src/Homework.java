149e985680b5c749a497759d66d2df9ea3c79a6c
bobralex2005 <u707755@gmail.com>
2017-07-03 21:18:43 +0300

    Still working

--- Java 2/Homework/Homework1/src/Homework.java
+++ Java 2/Homework/Homework1/src/Homework.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 /**
  * Created by Alexander Bugaev on 03.07.2017.
  */
 public class Homework {
+    public static void main(String[] args) {
+        Team team = new Team("Blue", Animal("Bob", 100, 5));
 
+    }
 }
